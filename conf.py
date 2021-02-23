310d734f75648656a35d2cd0c3129420a514d8c5
pavan1806 <79440519+pavan1806@users.noreply.github.com>
2021-02-23 16:44:38 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git and github First Project'
+copyright = '2021, Pavan'
+author = 'Pavan'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
